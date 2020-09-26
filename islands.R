aa942f385c4ed0bc904aad161bdb030410f71bb4
Jeremias Moraes <moraesjeremias@gmail.com>
2020-09-26 20:28:06 -0300

    Adding standard deviation and range evaluation

--- islands.R
+++ islands.R
@@ -1,10 +1,15 @@
 install.packages("ggplot2")
 library(ggplot2)
 
+
+#Etapa 1
 #Carregando os dados
 islands_dataset = data.frame(islands)
+islands$names = 
 print(islands_dataset)
 
+
+#Etapa 2
 #Soma das ocorrências
 sum(islands_dataset$islands)
 
@@ -13,3 +18,19 @@ mean(islands_dataset$islands)
 
 #Mediana das ocorrências
 median(islands_dataset$islands)
+
+
+#Etapa 3 
+#Retorna o menor valor de área (milhas quadradas) Islands
+min(range(islands_dataset, na.rm = TRUE))
+
+#Retorna o maior valor de área (milhas quadradas) Islands
+max(range(islands_dataset, na.rm = TRUE))
+
+#Etapa 4
+#Desvio Padrão das áreas (milhas quadradas) Islands
+sd(islands_dataset$islands)
+
+#Retorna a faixa das Ilhas
+range(islands_dataset$islands)
+
