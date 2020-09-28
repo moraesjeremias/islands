db13441f5ab750100933c0e83f5c7bdf0a0859f8
Jeremias Moraes <moraesjeremias@gmail.com>
2020-09-27 23:31:05 -0300

    Adding stem-and-leaf and box plot + outliers evaluation

--- islands.R
+++ islands.R
@@ -1,7 +1,6 @@
 install.packages("ggplot2")
 library(ggplot2)
 
-
 #Etapa 1
 #Carregando os dados
 islands_dataset = data.frame(islands)
@@ -11,17 +10,15 @@ colnames(islands_dataset)[names(islands_dataset) == "islands"] = "island_area"
 names(islands_dataset)
 print(islands_dataset)
 
-
-#Etapa 2
 #Quantidade de ocorrências de Ilhas
 length(islands_dataset$island_names)
 
+#Etapa 2
 #Média das ocorrências
 mean(islands_dataset$island_area)
 
 #Mediana das ocorrências
 median(islands_dataset$island_area)
-
 
 #Etapa 3 
 #Retorna o menor valor de área (milhas quadradas) Islands
@@ -30,7 +27,7 @@ min(range(islands_dataset$island_area, na.rm = TRUE))
 #Retorna o maior valor de área (milhas quadradas) Islands
 max(range(islands_dataset$island_area, na.rm = TRUE))
 
-#Etapa 4
+# Etapa 4
 #Desvio Padrão das áreas (milhas quadradas) Islands
 sd(islands_dataset$island_area)
 
@@ -54,12 +51,16 @@ print(partial_coverage_quantils)
 # - Quantil 1 a Quantil 5 (0% - 100%)
 IQR(islands_dataset$island_area)
 
-#plot
 
-
+#Etapa 7
+#Plot do Histograma de Frequência de Áreas das Ilhas
 ggplot(data = islands_dataset, aes(x=island_area))+
-  geom_histogram()
+  geom_histogram(binwidth = 5000)
 
+#Plot do Histograma de Frequência Relativa de Áreas das Ilhas
+ggplot(data = islands_dataset, aes(x=island_area))+
+  geom_histogram(aes(y=stat(count)/sum(count)),binwidth = 5000) +
+    scale_y_continuous(labels = scales::percent)
 
 #Etapa 8
 
@@ -67,11 +68,17 @@ ggplot(data = islands_dataset, aes(x=island_area))+
 outliers_plot = ggplot(data = islands_dataset, aes(y=island_area))+
   geom_boxplot()
 print(outliers_plot)
+
 #Box Plots sem outliers
 lower_upper_whiskers = boxplot.stats(islands_dataset$island_area)$stats[c(1,5)]
+print(lower_upper_whiskers)
 normalized_plot = outliers_plot + coord_cartesian(ylim = lower_upper_whiskers*1.05)
 print(normalized_plot)
-print(lower_upper_whiskers)
 
+#Etapa 9 
+#Encontrando Outliers
+outliers = boxplot.stats(islands_dataset$island_area)$out
+print(outliers)
+
+#Encontrando a representação do gráfico de folhas
 stem(islands_dataset$island_area)
-leaf(islands_dataset$island_area)
